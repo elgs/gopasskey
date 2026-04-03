163aebdd427b995ced6890ee4c10500f3fd9b9f7
Qian Chen <elgs1980@hotmail.com>
2026-04-03 02:27:48 -0700

    Update.

--- web/src/components/root/root.js
+++ web/src/components/root/root.js
@@ -7,32 +7,14 @@ customElements.define('web-root',
       super(ast);
     }
 
-    update() {
-      // no-op: prevents TreeWalker from entering child components
+    loggedIn = !!localStorage.getItem('sid');
+
+    onLogin() {
+      this.loggedIn = true;
     }
 
-    domReady() {
-      this.loginEl = document.createElement('web-login');
-      this.dashboardEl = document.createElement('web-dashboard');
-
-      this.loginEl.addEventListener('login', () => this.showDashboard());
-      this.dashboardEl.addEventListener('logout', () => this.showLogin());
-
-      if (localStorage.getItem('sid')) {
-        this.showDashboard();
-      } else {
-        this.showLogin();
-      }
-    }
-
-    showLogin() {
-      this.dashboardEl.remove();
-      this.appendChild(this.loginEl);
-    }
-
-    showDashboard() {
-      this.loginEl.remove();
-      this.appendChild(this.dashboardEl);
+    onLogout() {
+      this.loggedIn = false;
     }
   }
 );
